ee556cb8ef29a43ac4fc5deb077c0aa391d4e764
PeterKehoe <Peter.s.Kehoe@gmail.com>
2017-02-12 22:03:13 +0000

    Initial Profile Work
    Testing Git really

---
+++ Profiles/Device Profile.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 08 21:01:51 2017
+
+@author: Peter
+"""
+
+class device():
+    
+    def __init__(self,name,ofv,rs):
+        self.ID = name
+        self.OFversion = ofv
+        self.RuleSize = rs
+        self.ports = {}
+        self.linked = {} 
+    
+    def addLink(self,portNumber, connectedTo, farPort):
+        if portNumber not in self.linked:
+            self.linked[portNumber] = None
+        if self.linked[portNumber] is not connectedTo:
+            self.linked[portNumber] = connectedTo
+            connectedTo.addLink(farPort, self, portNumber)
+                
+            
+            
